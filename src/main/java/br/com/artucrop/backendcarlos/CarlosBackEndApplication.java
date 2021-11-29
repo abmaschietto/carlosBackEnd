99cda87c92703b161139ad489cdc45e91d1d8db7
abmaschietto <arturboudoux@gmail.com>
2021-11-29 19:34:18 -0300

    Setup inicial

---
+++ src/main/java/br/com/artucrop/backendcarlos/CarlosBackEndApplication.java
@@ -0,0 +1,13 @@
+package br.com.artucrop.backendcarlos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarlosBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarlosBackEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/artucrop/backendcarlos/CarlosBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.artucrop.backendcarlos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarlosBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
